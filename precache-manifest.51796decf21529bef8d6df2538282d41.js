e1db036af41e57193329502c2d7955edaafe77c5
Stefan Alin <stefanalindascalescu@outlook.com>
2019-11-29 15:16:01 +0200

    Updates

---
+++ precache-manifest.51796decf21529bef8d6df2538282d41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16356d052e9b2a2badc6f78758d8065",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8d212706a4a86d024227",
+    "url": "/monsters-rolodex/static/css/main.2d254b36.chunk.css"
+  },
+  {
+    "revision": "7455cde9cdf47cc0a1bc",
+    "url": "/monsters-rolodex/static/js/2.78d32658.chunk.js"
+  },
+  {
+    "revision": "8d212706a4a86d024227",
+    "url": "/monsters-rolodex/static/js/main.a9f9aed9.chunk.js"
+  },
+  {
+    "revision": "0dfc03588814ff7edbe7",
+    "url": "/monsters-rolodex/static/js/runtime-main.4cd1dc24.js"
+  }
+]);
